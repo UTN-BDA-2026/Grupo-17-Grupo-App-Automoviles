b8520a569bd2d266e713006b6959823901f62a29
Juan Cruz Diaz <djuancruz21@gmail.com>
2026-05-06 18:55:57 -0300

    feat(scraper): add link entity in scraper module

---
+++ backend/src/scraper/link.entity.ts
@@ -0,0 +1,43 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, OneToOne, JoinColumn } from 'typeorm';
+import { Listing } from '../products/listing.entity';
+import { Store } from '../products/store.entity';
+
+export enum LinkStatus {
+    PENDIENTE = 'Pendiente',
+    PROCESANDO = 'Procensando',
+    COMPLETADO = 'Completado',
+}
+
+@Entity('links')
+export class Link {
+
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column({ type: 'uuid', nullable: false})
+    store_id!: string;
+
+    @Column({ type: 'enum', enum: LinkStatus, nullable: false})
+    status!: LinkStatus
+
+    @Column({ type: 'text', nullable: false})
+    url!: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at!: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updated_at!: Date;
+
+    @ManyToOne(() => Store, (store) => store.links, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    })
+    @JoinColumn({ name: 'store_id' })
+    store!: Store;
+
+    @OneToOne(() => Listing, (listing) => listing.link)
+    listing!: Listing
+
+}
