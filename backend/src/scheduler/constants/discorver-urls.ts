162646931314569eeb1c2b12a631d1090abeb4aa
Juan Cruz Diaz <djuancruz21@gmail.com>
2026-05-07 23:51:10 -0300

    feat(scraper): create a constant with base urls to discover and set this in the respective service

---
+++ backend/src/scheduler/constants/discorver-urls.ts
@@ -0,0 +1,9 @@
+
+export const DISCOVER_URLS = [
+    'https://autos.mercadolibre.com.ar/ford/f-100',
+    'https://autos.mercadolibre.com.ar/ford/f-150-raptor',
+    'https://autos.mercadolibre.com.ar/ford/ranger',
+    'https://autos.mercadolibre.com.ar/ford/ka',
+    'https://autos.mercadolibre.com.ar/ford/falcon',
+    'https://autos.mercadolibre.com.ar/ford/fiesta',
+]
