cc8f0bdaaefff4613ef9a5cf2621d82b5aaaf5cc
Macarena Damiani <macarenadamiani@gmail.com>
2026-05-09 11:46:20 -0300

    users module finished

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { User } from './user.entity';
 
-@Module({})
-export class UsersModule {}
+@Module({
+    imports: [TypeOrmModule.forFeature([User])],
+    controllers: [UsersController],
+    providers: [UsersService],
+})
+
+export class UsersModule { }
