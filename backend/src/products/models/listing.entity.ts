3d407216322b930e77c05bebdbb2e3a5b6c16860
Juan Cruz Diaz <djuancruz21@gmail.com>
2026-05-17 11:13:44 -0300

    feat(products): move products entities from root module to new models directory

---
+++ backend/src/products/models/listing.entity.ts
@@ -0,0 +1,53 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToOne, ManyToOne, JoinColumn } from 'typeorm';
+import { Vehicle } from './vehicle.entity';
+import { Link } from '../../scraper/link.entity';
+
+@Entity('listings')
+export class Listing {
+
+    @PrimaryGeneratedColumn('uuid')
+    id!: string; 
+
+    @Column({ type: 'uuid' })
+    vehicle_id!: string;
+
+    @Column({ type: 'uuid' })
+    store_id!: string;
+
+    @Column({ type: 'varchar', length: 255, unique: true })
+    external_id!: string;
+
+    @Column({ type: 'decimal', precision: 15, scale: 2 })
+    price!: number;
+
+    @Column({ type: 'varchar', length: 10 })
+    currency!: string;
+
+    @Column({ type: 'int', nullable: true })
+    mileage!: number;
+
+    @Column({ type: 'text' })
+    external_url!: string;
+
+    @Column({ type: 'boolean', default: true })
+    is_available!: boolean;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at!: Date;
+
+    @Column({ type: 'timestamp' })
+    last_scraped!: Date;
+
+    @ManyToOne(() => Vehicle, (vehicle) => vehicle.listings, { 
+        onDelete: 'RESTRICT',
+        onUpdate: 'CASCADE'
+    })
+    @JoinColumn({ name: 'vehicle_id' })
+    vehicle!: Vehicle;
+
+    @OneToOne(() => Link, (link) => link.listing)
+    @JoinColumn()
+    link!: Link;
+
+}
---
+++ backend/src/products/models/store.entity.ts
@@ -0,0 +1,26 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Link } from '../../scraper/link.entity';
+
+@Entity('stores')
+export class Store {
+
+    @PrimaryGeneratedColumn('uuid')
+    id!: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    name!: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    base_url!: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    created_at!: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    updated_at!: Date;
+
+    @OneToMany(() => Link, (links) => links.store)
+    links!: Link[];
+
+}
