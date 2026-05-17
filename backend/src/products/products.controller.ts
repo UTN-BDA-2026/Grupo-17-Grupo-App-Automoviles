2130f241d53eb1f9e92fc8bf1cf2bcbe6b3e7d78
Pablo Maturano <pablomaturanoo04@gmail.com>
2026-05-17 11:35:12 -0300

    feat(products): add products controller with 2 endpoints, get and getById

---
+++ backend/src/products/products.controller.ts
@@ -0,0 +1,29 @@
+
+import { Controller, Get, Param, ParseUUIDPipe, Query, DefaultValuePipe, ParseIntPipe } from '@nestjs/common';
+import { ListingService } from './services/listing.service';
+
+@Controller('products')
+export class ProductsController {
+
+    constructor(private readonly listingService: ListingService){}
+
+    @Get()
+    public async getListing(
+        @Query('pageNumber', new DefaultValuePipe(1), ParseIntPipe) pageNumber: number,
+        @Query('pageSize', new DefaultValuePipe(10), ParseIntPipe) pageSize: number
+    ) {
+
+        const result = this.listingService.get(pageNumber, pageSize)
+        return result 
+
+    }
+
+    @Get('/:id')
+    public async getListingById(@Param('id', new ParseUUIDPipe({errorHttpStatusCode: 400})) id : string) {
+
+        const result = this.listingService.getById(id)
+        return result 
+
+    }
+
+}
