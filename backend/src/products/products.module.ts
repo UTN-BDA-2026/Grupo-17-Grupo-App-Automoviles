d7ec823be7532e9433e817a6cd6304bdbea5b72a
Juan Cruz Diaz <djuancruz21@gmail.com>
2026-05-17 11:16:20 -0300

    feat(products): update module file with new providers (services)

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,4 +1,10 @@
-import { Module } from '@nestjs/common';
 
-@Module({})
+import { Module } from '@nestjs/common';
+import { ProductsController } from './products.controller';
+import { ListingService } from './services/listing.service';
+
+@Module({
+  controllers: [ProductsController],
+  providers: [ListingService]
+})
 export class ProductsModule {}
